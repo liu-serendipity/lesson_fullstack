09ea6ffe8e05cca468d4530dd093db664139f533
liu-serendipity <serendipity.liulimin@gmail.com>
2022-06-16 00:46:24 +0800

    react/cart-demo/

---
+++ react/cart-demo/src/App.jsx
@@ -0,0 +1,14 @@
+import { useState } from 'react'
+import 'antd/dist/antd.css';
+import Cart from './components/cart/index'
+
+function App() {
+
+  return (
+    <div className="App">
+      <Cart />
+    </div>
+  )
+}
+
+export default App
