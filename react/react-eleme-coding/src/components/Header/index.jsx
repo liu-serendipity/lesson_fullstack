7a1d7958badbd9a3de0f633b7685356d62650a65
liu-serendipity <serendipity.liulimin@gmail.com>
2022-06-16 16:53:21 +0800

    header组件使用useLocation

--- react/react-eleme-coding/src/components/Header/index.jsx
+++ react/react-eleme-coding/src/components/Header/index.jsx
@@ -1,28 +1,20 @@
-import React, { useEffect, useState } from 'react'
+import React, {useEffect, useState } from 'react'
 import { useLocation } from 'react-router-dom'
-import { pageTitle } from '../../config/index'
 
-const Header = () => {
-    // const { title="饿了么" } = props
-    const [title, setTitle] = useState("首页")
-    const { pathname="/" } = useLocation()
-    // console.log(location);
+export default function Header(props) {
+    const [title, setTitle] = useState('首页')
+    const {pathname="/"} = useLocation()
+
     useEffect(() => {
-        // const location = window.location.pathname;
-        // console.log(location);
-        let _title = pageTitle[pathname] || ''
-        // if (pathname == '/order') {
-        //     _title = '订单'
-        // } else if (pathname == '/home' || pathname == '/') {
-        //     _title = '首页'
-        // }
+        let _title;
+        if (pathname == '/order') {
+            _title = '订单'
+        }  else if (pathname == '/home' || pathname == '/') {
+            _title = '首页'
+        }
         setTitle(_title)
     })
-    return (
-        <div>
-            {title}
-        </div>
-    )
+  return (
+    <header>{title}</header>
+  )
 }
-
-export default Header
